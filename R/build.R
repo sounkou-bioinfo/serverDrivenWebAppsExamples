6316b7b5950c23de7dedaf79e8afd4b3cb45c7ec
sounkou-bioinfo <sounkoutoure@gmail.com>
2025-03-03 23:44:35 +0400

    some bizarre " Must return a response" error when updating the dog df

--- R/build.R
+++ R/build.R
@@ -10,12 +10,12 @@
 build <- \(...) {
 
   # future mirai 
-  library(future.mirai)
-  plan(mirai_multisession)
-  # create app instance
+  #library(future.mirai)
+  #plan(mirai_multisession)
  
+ 
+  # create app instance
   app <- ambiorix::Ambiorix$new()
-
   # 404 page
   app$not_found <- render_404
 
@@ -25,32 +25,33 @@ build <- \(...) {
   # serve static files
   app$static(assets_path(), "static")
 
-
+  # homepage
   app$get("/", landing_get)
 
-  # homepage
-  app$get("/ambiorix", ambiorix_get)
-
   # version
-
+  app$get("/ambiorix", ambiorix_get)
   app$get("/version", ambiorix_version_get)
-
   # about
   app$get("/about", about_get)
 
   # contact
   app$get("/contact", contact_get)
-
   app$post("/contact", contact_post)
+  
+  # simple crud
+  app$get("/crud", crud_page_get)
+  app$post("/dog", dog_post)
+  app$get("/table-rows", table_rows_get)
+  app$delete("/dog/:id", dog_delete)
+
 
   # test async
-
-  app$get("/async", \(req, res){
-  future({
-    Sys.sleep(1)
-    res$sendf(Sys.time() |> as.character())
-  })
-})
+  #app$get("/async", \(req, res){
+  #future({
+  #  Sys.sleep(1)
+  #  res$sendf(Sys.time() |> as.character())
+  #})
+#})
 
 
   return(app)
