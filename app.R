7716ac43828673a371f9e2a5387ca96f76c1cf31
sounkou-bioinfo <sounkoutoure@gmail.com>
2025-03-03 18:45:11 +0400

    remove do

--- app.R
+++ app.R
@@ -1,10 +1,14 @@
 
 dotenv::load_dot_env(file = ".env")
 
+
+#--- Set port
 AMBIORIX_PORT <- Sys.getenv(
         "AMBIORIX_PORT",
-        1995L) |>
-        as.integer()
+        "1995") 
+# check if within shinyserver runner
+AMBIORIX_PORT  <- Sys.getenv("SHINY_PORT", AMBIORIX_PORT)
+AMBIORIX_PORT <- as.integer(AMBIORIX_PORT)
 # load package functions
 pkgload::load_all()
 # run the app
